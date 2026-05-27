aed649e40ededec6ed66cac2df9146964e83e1ef
Manoel <vinesouza9x8@gmail.com>
2026-05-27 15:36:33 -0300

    feat: Nova modicacao

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Quase morri pq meu pc deu tela preta!");
+        System.out.printf("É apenas uma fase. Cuide bem de seu computador.");
+
+        System.out.printf("Aprova ai minha modificação. :)");
     }
 }
