2be068a30406808923276b3e63a926752fd233c5
Manoel <vinesouza9x8@gmail.com>
2026-05-27 16:29:30 -0300

    feat: so funciona em javas mais novos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Quase morri pq meu pc deu tela preta!");
+        System.out.printf("É apenas uma fase. Cuide bem de seu computador.");
+
+        System.out.printf("Aprova ai minha modificação. :)");
+
+        System.out.println("Modificação nova: ");
+
+                System.out.println(""" Se for java antigo o gatinho não
+                Hello World
+
+                           /\     /\
+                          {  `---'  }
+                          {  O   O  }
+~~~~~~~~~~~~~~~~~~~~~~~~~~|~   V   ~|~~~~~~~~~~~~~~~~~~~~~~~~~~
+                           \  \|/  /
+                            `-----'____
+                            /     \    \_
+                           {       }\  )_\_   _
+                           |  \_/  |/ /  \_\_/ )
+                            \__/  /(_/     \__/
+                              (__/
+                """);
+        
     }
 }
